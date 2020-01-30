f8ada077d5467f025657dc9cacc457cbcb9b336d
gmediaiosdev2 <maulana.husni@gmedia.co.id>
2020-01-30 09:17:24 +0700

    fix ontime, upload to playstore

--- app/src/main/java/gmedia/net/id/OnTime/menu_pengajuan/MenuPengajuan.java
+++ app/src/main/java/gmedia/net/id/OnTime/menu_pengajuan/MenuPengajuan.java
@@ -71,29 +71,29 @@ public class MenuPengajuan extends AppCompatActivity {
         Field tabStrip = null;
         try {
             tabStrip = tabLayout.getDeclaredField("mTabStrip");
+
+            tabStrip.setAccessible(true);
+            LinearLayout llTab = null;
+            try {
+                llTab = (LinearLayout) tabStrip.get(tabs);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+
+            int left = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, leftDip, Resources.getSystem().getDisplayMetrics());
+            int right = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, rightDip, Resources.getSystem().getDisplayMetrics());
+
+            for (int i = 0; i < llTab.getChildCount(); i++) {
+                View child = llTab.getChildAt(i);
+                child.setPadding(0, 0, 0, 0);
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1);
+                params.leftMargin = left;
+                params.rightMargin = right;
+                child.setLayoutParams(params);
+                child.invalidate();
+            }
         } catch (NoSuchFieldException e) {
             e.printStackTrace();
-        }
-
-        tabStrip.setAccessible(true);
-        LinearLayout llTab = null;
-        try {
-            llTab = (LinearLayout) tabStrip.get(tabs);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-
-        int left = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, leftDip, Resources.getSystem().getDisplayMetrics());
-        int right = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, rightDip, Resources.getSystem().getDisplayMetrics());
-
-        for (int i = 0; i < llTab.getChildCount(); i++) {
-            View child = llTab.getChildAt(i);
-            child.setPadding(0, 0, 0, 0);
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1);
-            params.leftMargin = left;
-            params.rightMargin = right;
-            child.setLayoutParams(params);
-            child.invalidate();
         }
     }
 }
